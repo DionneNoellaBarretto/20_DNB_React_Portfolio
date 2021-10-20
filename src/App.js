c81662312812de3bb699d6ad931b198485823ace
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-10-20 15:54:33 -0400

    responsive ui structure and color selection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-    {/* since we have 2 sections on the main page diving up the width of the sections using divs and col notation as in bootstrap */}
-      <div className="col-3 sidebar"> Sidebar</div>
-      <div className="col-9 mainComponent"> Main </div>
+      {/* since we have 2 sections on the main page diving up the width of the sections using divs and col notation as in bootstrap */}
+      <div className="container">
+        <div className="row">
+        {/* for large screens the width of sidebar is upto 3 cols anything smaller will occupy the full screen!  */}
+          <div className="col-lg-3"> 
+            <div className="sidebar">Sidebar</div>
+          </div>
+          <br/>
+          <div className="col-lg-9 mainComponent"> Main </div>
+        </div>
+      </div>
     </div>
   );
 }
