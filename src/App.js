84d37e7d851cd36e3e2944a13ac53080e7f01dc8
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-10-19 20:35:27 -0400

    structure of main page divided into 2 unequal sections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+    {/* since we have 2 sections on the main page diving up the width of the sections using divs and col notation as in bootstrap */}
+      <div className="col-3 sidebar"> Sidebar</div>
+      <div className="col-9 mainComponent"> Main </div>
     </div>
   );
 }
