50e2d1ab672a0ab3f6af0255e58cfe4bd80c0280
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-10-23 16:01:53 -0400

    contact page setup completed..

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ import cal from "../assets/icons/cal.png";
 import Google_Voice_icon from "../assets/icons/Google_Voice_icon.png";
 import mail from "../assets/icons/mail.png";
 
-// as seen https://javascript.plainenglish.io/how-to-build-a-contact-form-in-react-that-sends-emails-using-emailjs-70011d2563a3
+// as seen in https://getbootstrap.com/docs/4.1/components/forms/ &  https://javascript.plainenglish.io/how-to-build-a-contact-form-in-react-that-sends-emails-using-emailjs-70011d2563a3
 import "../index.css";
 
 const SERVICE_ID = "service_xbpztyp";
@@ -42,44 +42,54 @@ const Contact = () => {
     window.open("mailto:dionnenoellabarretto.aw@gmail.com");
   };
 
-  /* --- handleOnSubmit method --- */
 
   return (
     <div className="row">
       <br />
       <div className="col-lg-5 text-center Contact text-center">
+      {/* --- handleOnSubmit method --- */}
         <Form className="contactForm form-text " onSubmit={handleOnSubmit}>
           <Form.Field
-            id="form-input-control-email"
+            id="form-input-control-email validationTooltip01"
             control={Input}
-            label="Your Email"
+            for="validationTooltip01"
+            label ="Your Email"
             title="Your email ID will never be shared with anyone else"
             name="user_email"
             placeholder="Your Email…"
             required
-            className="col-lg-12 col-form-label-lg"
-          />
+            className="col-lg-12 col-form-label-lg needs-validation"
+          /><div className="valid-tooltip">
+        Looks good!
+      </div>
           <br />
           <Form.Field
-            id="form-input-control-last-name"
+            id="form-input-control-last-name validationTooltip01"
             control={Input}
+            for="validationTooltip01"
             label="Your Name"
             name="user_name"
             placeholder="Your Name…"
             title="Your name is being requested so I can address you correctly in my response back!"
             required
-            className="col-lg-12 col-form-label-lg"
-          />
+            className="col-lg-12 col-form-label-lg validationTooltip01 needs-validation"
+          /><div className="valid-tooltip">
+        Looks good!
+      </div>
           <br />
           <Form.Field
-            id="form-textarea-control-opinion"
+            id="form-textarea-control-opinion validationTooltip01"
             control={TextArea}
+            for="validationTooltip01"
             label="Your Message"
             name="user_message"
             placeholder="Your Message…"
+            title="Please provide enough context in your message, so I can respond back to you swiftly ! :) "
             required
-            className="col-lg-12 col-form-label-lg"
-          />
+            className="col-lg-12 col-form-label-lg needs-validation"
+          /><div className="valid-tooltip">
+        Looks good!
+      </div>
           <Button type="submit">Submit</Button>
         </Form> 
       </div> 
@@ -99,7 +109,7 @@ const Contact = () => {
                 className="sidebar_icon"
               />
             </a>
-          </div>
+          </div><hr />
           <div className="col-lg-11 sidebar_items">
             <h6> Email Dionne Noella Barretto </h6>
             <a
@@ -116,7 +126,7 @@ const Contact = () => {
                 className="sidebar_icon"
               />
             </a>
-          </div>
+          </div><hr />
           <div className="col-lg-11 sidebar_items">
             <h6> Call Dionne Noella Barretto </h6>
             <a
@@ -135,6 +145,8 @@ const Contact = () => {
           </div>
         </div>
       </div>
+
+
 
   );
 };
