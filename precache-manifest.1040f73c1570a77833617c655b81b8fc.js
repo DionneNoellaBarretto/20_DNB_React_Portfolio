af9ef45726cda19ab63e3730f54c21d414c052ba
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-10-25 22:09:43 -0400

    Updates

---
+++ precache-manifest.1040f73c1570a77833617c655b81b8fc.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd2143d17951c87f0f2abf613706376",
+    "url": "/20_DNB_React_Portfolio/index.html"
+  },
+  {
+    "revision": "bcbeb0f6a05c67225405",
+    "url": "/20_DNB_React_Portfolio/static/css/main.7473ea9c.chunk.css"
+  },
+  {
+    "revision": "5bae7f68c485f3eb8a8d",
+    "url": "/20_DNB_React_Portfolio/static/js/2.6577c6dc.chunk.js"
+  },
+  {
+    "revision": "24614dc3b68f7400393248f6043989cd",
+    "url": "/20_DNB_React_Portfolio/static/js/2.6577c6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbeb0f6a05c67225405",
+    "url": "/20_DNB_React_Portfolio/static/js/main.62e4ae82.chunk.js"
+  },
+  {
+    "revision": "5da0b19cb1c4a98043fd",
+    "url": "/20_DNB_React_Portfolio/static/js/runtime-main.2e67b1bc.js"
+  },
+  {
+    "revision": "2cbbab1cb3a887f180a840b94c20a576",
+    "url": "/20_DNB_React_Portfolio/static/media/Airwatch.2cbbab1c.png"
+  },
+  {
+    "revision": "247e0263e2dcc92d86dc19dad6e7fe9f",
+    "url": "/20_DNB_React_Portfolio/static/media/CSSPortfolio.247e0263.png"
+  },
+  {
+    "revision": "30dbf1c1bc2d30da8900ab75bd263048",
+    "url": "/20_DNB_React_Portfolio/static/media/CodeQuiz.30dbf1c1.png"
+  },
+  {
+    "revision": "103fb66cfcc23346fd594b6ce38d0267",
+    "url": "/20_DNB_React_Portfolio/static/media/ECommerceBackendApp.103fb66c.gif"
+  },
+  {
+    "revision": "c6c91d0fa001104464519b783538d8c0",
+    "url": "/20_DNB_React_Portfolio/static/media/EmployeeContentManagementSystem.c6c91d0f.gif"
+  },
+  {
+    "revision": "816e344551d7daaa17dcf44121eb2c2b",
+    "url": "/20_DNB_React_Portfolio/static/media/Feedster.816e3445.png"
+  },
+  {
+    "revision": "010f254fd4706324468acaabba91ebe8",
+    "url": "/20_DNB_React_Portfolio/static/media/FitnessTracker.010f254f.png"
+  },
+  {
+    "revision": "153090f31daaa52a21af355ed246b45c",
+    "url": "/20_DNB_React_Portfolio/static/media/Gcloud.153090f3.gif"
+  },
+  {
+    "revision": "002a479d20f8fd8b1c07f0ff5ac508e7",
+    "url": "/20_DNB_React_Portfolio/static/media/Google.002a479d.gif"
+  },
+  {
+    "revision": "0b8173d0046fc2a7422079416d7dc3b4",
+    "url": "/20_DNB_React_Portfolio/static/media/Google_Voice_icon.0b8173d0.png"
+  },
+  {
+    "revision": "7ad8f465b5b3b962ff2c2cada2f73af7",
+    "url": "/20_DNB_React_Portfolio/static/media/HexColorExample.7ad8f465.png"
+  },
+  {
+    "revision": "baaf5e082810da09278ca35a413a3aab",
+    "url": "/20_DNB_React_Portfolio/static/media/ImFeelingApp.baaf5e08.png"
+  },
+  {
+    "revision": "5249ba64cfd77ea20ab6885d491a3b32",
+    "url": "/20_DNB_React_Portfolio/static/media/LandingPage.5249ba64.png"
+  },
+  {
+    "revision": "01603d0b82ddd11619452b7801a475e7",
+    "url": "/20_DNB_React_Portfolio/static/media/NoteTaker.01603d0b.png"
+  },
+  {
+    "revision": "b568719654d5f255bce1c859dc158ded",
+    "url": "/20_DNB_React_Portfolio/static/media/PasswordGenerator.b5687196.png"
+  },
+  {
+    "revision": "416968e27712da318037c3d32c8b932f",
+    "url": "/20_DNB_React_Portfolio/static/media/Professional_README.416968e2.gif"
+  },
+  {
+    "revision": "4c4ce9bba8055e2dae32379de24382c8",
+    "url": "/20_DNB_React_Portfolio/static/media/ProfileIDCard.4c4ce9bb.png"
+  },
+  {
+    "revision": "59d12f96550fc131c3751b20d3f987c9",
+    "url": "/20_DNB_React_Portfolio/static/media/RegexGist.59d12f96.png"
+  },
+  {
+    "revision": "95f2291b3810f8c03362b26385380e5a",
+    "url": "/20_DNB_React_Portfolio/static/media/Rubrik.95f2291b.png"
+  },
+  {
+    "revision": "5ba3faa4d564c2a3389d34656a8f359a",
+    "url": "/20_DNB_React_Portfolio/static/media/SimpleInterestCalculator.5ba3faa4.png"
+  },
+  {
+    "revision": "bdb83cfe10252d8a62ef470323fbb70b",
+    "url": "/20_DNB_React_Portfolio/static/media/TeamProfileGenerator.bdb83cfe.gif"
+  },
+  {
+    "revision": "6e99636041261287a1fafa43783c90f4",
+    "url": "/20_DNB_React_Portfolio/static/media/TributePage.6e996360.png"
+  },
+  {
+    "revision": "a6496f02a6353391b23a9abae01850d6",
+    "url": "/20_DNB_React_Portfolio/static/media/UpdatedPortfolio.a6496f02.png"
+  },
+  {
+    "revision": "ca81d218107842605c60a42b7adccd7d",
+    "url": "/20_DNB_React_Portfolio/static/media/WeatherDashboard.ca81d218.png"
+  },
+  {
+    "revision": "58ac708f25f560c5d35986dd9f2471b4",
+    "url": "/20_DNB_React_Portfolio/static/media/WorkDayScheduler.58ac708f.png"
+  },
+  {
+    "revision": "49aa659c3e2c21d9882f79509769741f",
+    "url": "/20_DNB_React_Portfolio/static/media/cert.49aa659c.png"
+  },
+  {
+    "revision": "fcc7eaeaca41cfb4d3b4ab6b66e39fee",
+    "url": "/20_DNB_React_Portfolio/static/media/codecademy.fcc7eaea.svg"
+  },
+  {
+    "revision": "e6c785b975226523a3196acbb0820c2a",
+    "url": "/20_DNB_React_Portfolio/static/media/fcc.e6c785b9.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/20_DNB_React_Portfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "615bf9eebfe56e33254ead97b7bb330e",
+    "url": "/20_DNB_React_Portfolio/static/media/leetcode.615bf9ee.png"
+  },
+  {
+    "revision": "68b4983e486ce70f00b9dc9dc7eafd34",
+    "url": "/20_DNB_React_Portfolio/static/media/mail.68b4983e.png"
+  },
+  {
+    "revision": "d36127974c4bdd3f0490d28604316639",
+    "url": "/20_DNB_React_Portfolio/static/media/pin.d3612797.png"
+  },
+  {
+    "revision": "31e76df5b95eaa414b7660faae4b59a3",
+    "url": "/20_DNB_React_Portfolio/static/media/qwiklabs.31e76df5.png"
+  },
+  {
+    "revision": "96ca3ee0f673c1198b11abda76afd3ed",
+    "url": "/20_DNB_React_Portfolio/static/media/resume.96ca3ee0.png"
+  },
+  {
+    "revision": "ef7627ff13c5cc86b304f04db038a047",
+    "url": "/20_DNB_React_Portfolio/static/media/vmware.ef7627ff.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/20_DNB_React_Portfolio/precache-manifest.1040f73c1570a77833617c655b81b8fc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/20_DNB_React_Portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
